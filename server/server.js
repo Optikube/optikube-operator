1150a3dacb54e20b46336afa76742560ee075a17
James Shea <jshea915@gmail.com>
2024-03-09 13:59:30 -0700

    basic operator and server setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,48 @@
-const http = require('http');
+const express = require('express');
+const evaluateCostAndUpdateHPA = require('./operator.js')
 
-const hostname = '0.0.0.0';
+const app = express();
+
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+let isRunning = false;
+let costExceedsThreshold = false;
+let lowerLimit = 50;
+let upperLimit = 80;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const continuousEvaluateCostAndUpdateHPA = async () => {
+  while (isRunning) {
+    await evaluateCostAndUpdateHPA(costExceedsThreshold, lowerLimit, upperLimit);
+    // delay between invocation
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
+}
+
+app.get('/start', (req, res) => {
+  // invoke cost calculation and invoke hpa autoscale
+  if (!isRunning) {
+    isRunning = true;
+    continuousEvaluateCostAndUpdateHPA().catch(err => console.error(err));
+    res.status(200).send('Autoscaling started.');
+  } else {
+    res.status(200).send('Autoscaling already running.');
+  }
+})
+
+app.get('/stop', (req, res) => {
+  isRunning = false;
+  res.status(200).send('Autoscaling stopped.')
+})
+
+app.get('/adjust', (req, res) => {
+  if (req.query.costExceedsThreshold !== undefined) {
+    costExceedsThreshold = req.query.costExceedsThreshold === 'true';
+}
+  lowerLimit = req.query.lowerLimit ? parseInt(req.query.lowerLimit) : lowerLimit;
+  upperLimit = req.query.upperLimit ? parseInt(req.query.upperLimit) : upperLimit;
+  res.status(200).send('Parameters adjusted.')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}...`);
 });
