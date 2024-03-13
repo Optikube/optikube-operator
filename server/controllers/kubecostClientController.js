d1f8c0a0d5f7246cb8a774f925f23c5df1c20111
Andy Guajardo <andymatt1225@gmail.com>
2024-03-12 23:21:35 -0700

    Kubecost successful API calls for specific pod -able to call allocation API -retrieve specfic metrics from specific pods

--- server/controllers/kubecostClientController.js
+++ server/controllers/kubecostClientController.js
@@ -1,27 +1,60 @@
 // implement functions for fetching data from Kubecost APIs
 const axios = require('axios');
+const { stringify } = require('querystring');
+
+
+//pods tend to die, may need to change out w new pod name
+const theChosenPod = "optikube-deployment-5f77bdfd84-5js5x"
+
 
 const kubecostAllocationData = async (req, res, next) => {
-    try {
-        console.log('are we even here?');
-        const allocationData = await axios.get('http://localhost:9090/model/allocation', {
-            params: {
-                window: '6h',
-                aggregate: 'namespace',
-            }
-            });
-            
-            res.locals.allocation = allocationData;
-            console.log('after axios req in middleware')
-            return next();
+   try {
+       console.log('entering try block of kubecostAllo middleware');
+       const allocationData = await axios.get('http://localhost:9090/model/allocation', {
+           params: {
+               window: '1h',
+               aggregate: 'pod',
+               // filter: 'namespace: "optikube-namespace"',
+               // filter: 'pod: "optikube-deployment-5f77bdfd84-5js5x"',
+               filter: `pod:"${theChosenPod}"`,
+               // fields: 'cpuCost,ramBytes'
+           }
+           });
+           //this gives an object of only our pod and given metrics
+           const podObject = allocationData.data.data[0][theChosenPod]
+           // console.log('origial allocationData slimmed down->', podObject);
 
-    }  catch (error) {
-        console.log('There was an error in the get request for the allocation API', error);
-        res.status(500).json({ message: 'Error fetching allocation data' });
-    }
+
+           //no need to iterate now, add whatever other metrics we want
+           const jawshData = {
+               loadBalancerCost : podObject['loadBalancerCost'],
+               ramCost : podObject['ramCost'],
+               totalCost : podObject['totalCost']
+           };
+           console.log('Jawsh Obj w key/value label pairs -->', jawshData);
+          
+
+
+           //accessing allocationData JSON Obj directly and pulling vital metrics
+           // console.log('Load Balance Cost -->',allocationData.data.data[0][theChosenPod]["loadBalancerCost"]);
+           // console.log('RAM Cost -->',allocationData.data.data[0][theChosenPod]["ramCost"]);         
+           // console.log('Total Cost -->',allocationData.data.data[0][theChosenPod]["totalCost"]);
+
+
+           res.locals.allocation = allocationData;
+           // console.log('after axios req in middleware')
+           return next();
+
+
+   }  catch (error) {
+       console.log('There was an error in the get request for the allocation API', error);
+       res.status(500).json({ message: 'Error fetching allocation data' });
+   }
 };
+
 
 module.exports = {
-    kubecostAllocationData,
+   kubecostAllocationData,
 };
+
 
