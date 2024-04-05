947c20cb3058a7f0f179754f01998ab5ae4d22d3
Josh <josh.r.bajarias@gmail.com>
2024-04-05 00:20:30 -0400

    code: reorganized and refreshed codebase to represent current state.
    Co-authored-by: Andy Guajardo <andymattgee@users.noreply.github.com
    
    Co-authored-by: Cheryl Lee <yli-yanchen@user.noreply.github.com

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx';
+import App from '../App.jsx';
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    
+    <React.StrictMode>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
-    
+    </React.StrictMode>
 );
